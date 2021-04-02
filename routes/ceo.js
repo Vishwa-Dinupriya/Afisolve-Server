e2611694e3c7c2b8477ba44e7e5420ae665181fd
Buddhi <Buddhiravihansa1997@gmail.com>
2021-04-02 21:35:10 +0530

    after email

--- routes/ceo.js
+++ routes/ceo.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
 const router = express.Router();
+const nodemailer = require('nodemailer');
 
 const {poolPromise} = require('../helpers/mssql-server-connection');
 const {sql} = require('../helpers/mssql-server-connection');
@@ -303,7 +304,60 @@ router.post('/update-reminder', verifyToken, async (request, response)=> {
     const data = request.body;
     const charithe= 'CEO';
     const whaction= 'Reminder';
-    console.log(data)
+    const acemail = data.accountCoordinatorEmail;
+    const comid = data.complainID;
+    console.log(acemail);
+
+
+    // .............................................................
+    async function main() {
+        // Generate test SMTP service account from ethereal.email
+        // Only needed if you don't have a real mail account for testing
+        let testAccount = await nodemailer.createTestAccount();
+
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+             host: "smtp.ethereal.email",
+            // service: "gmail",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: testAccount.user, // generated ethereal user
+                pass: testAccount.pass, // generated ethereal password
+            },
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: '"Fred Foo 👻" <foo@example.com>', // sender address
+            to: acemail, // list of receivers
+            subject: "Reminder", // Subject line
+            text: "You have not complete providing solution for "  + {comid} +" complaint. please, complete your work quickly", // plain text body
+            html: "You have not complete providing solution for " + {comid} + " complaint. please, complete your work quickly", // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    }
+
+    main().catch(console.error);
+
+
+
+
+
+
+
+
+    //..........................................................................
+
+
+
+
     try {
         const pool = await poolPromise;
         pool.request()
