cbce44fccbf405e1d5bade0506386ef1592c6d6f
vishw <vishwadinupriya@gmail.com>
2021-05-15 16:18:48 +0530

    bug fixing continued... (test commitment)

--- routes/admin.js
+++ routes/admin.js
@@ -18,7 +18,7 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/get-all-users-details', verifyToken, verifyAdmin, async (request, response) => {
-
+    console.log('test');
     const pool = await poolPromise;
     try {
         pool.request()
